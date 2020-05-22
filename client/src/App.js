47cbb61e6e42583daf86d976fa73cf21a542f173
jo-ewa <11009431+jo-ewa@users.noreply.github.com>
2020-05-22 07:02:29 -0600

    set up player but having issue with clientId auth endpoint and params

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,55 @@
 import React, { Component } from "react";
 import hash from "./hash";
+import * as $ from "jquery";
 import logo from "./logo.svg";
 import "./App.css";
+import Player from './Player'
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "081df2bd9f3f4cc695277a3711ab3d56";
-const redirectUri = "http://localhost:3000";
+const redirectUri = "http://localhost:3000/";
 const scopes = [
+  "user-top-read",
   "user-read-currently-playing",
   "user-read-playback-state",
 ];
-// Get the hash of the url
-const hash = window.location.hash
-  .substring(1)
-  .split("&")
-  .reduce(function (initial, item) {
-    if (item) {
-      var parts = item.split("=");
-      initial[parts[0]] = decodeURIComponent(parts[1]);
-    }
-    return initial;
-  }, {});
-
-window.location.hash = "";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+      item: {
+        album: {
+          images: [{ url: "" }]
+        },
+        name: "",
+        artists: [{ name: "" }],
+        duration_ms: 0,
+      },
+      is_playing: "Paused",
+      progress_ms: 0
+    };
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+  }
+  getCurrentlyPlaying(token) {
+    // Make a call using the token
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player",
+      type: "GET",
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
+      success: (data) => {
+        this.setState({
+          item: data.item,
+          is_playing: data.is_playing,
+          progress_ms: data.progress_ms,
+        });
+      }
+    });
+  }
+
   componentDidMount() {
     // Set token
     let _token = hash.access_token;
@@ -50,8 +75,12 @@ class App extends Component {
             </a>
           )}
           {this.state.token && (
-        // Spotify Player Will Go Here In the Next Step
-      )}
+            <Player
+              item={this.state.item}
+              is_playing={this.state.is_playing}
+              progress_ms={this.progress_ms}
+            />
+          )}
         </header>
       </div>
     );
